db13b42f043ab60a432a66fdcc10a57c3c92e541
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2024-02-07 09:04:31 +0530

    Boolean

--- 1program.py
+++ 1program.py
@@ -1,6 +1,8 @@
+#print function
 print("Hello")
 a=10
 print(a)
 a=34.5
 print(a)
+
 
---
+++ boolen.py
@@ -0,0 +1,11 @@
+#Boolean
+a=True
+if(a==True):
+    print("I am Abhijeet")
+
+Multiplication of a & b;
+
+a=10
+b=5
+c=a*b
+print(c)
