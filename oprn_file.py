1c1997f88c1cf3cb6437b27ece7fa96ce101bb25
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2024-03-06 08:57:31 +0530

    commit

--- oprn_file.py
+++ oprn_file.py
@@ -4,3 +4,20 @@ with open('inputfile.txt',  encoding="utf-8") as f:
         if not line :
             break
         print(line)
+
+
+
+#Methods of File Objects 
+
+with open('inputfile.txt', encoding="utf-8") as f: 
+
+    for line in f: 
+
+        print(line, end='') 
+
+ 
+ 
+
+with open('inputfile.txt', 'w+', encoding="utf-8") as f: 
+
+    f.write('This is a test\n') 
