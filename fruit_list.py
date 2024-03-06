67711b0c0317430828ac68be1f681b757a7d6e85
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2024-03-06 09:43:40 +0530

    count number of fruits

---
+++ fruit_list.py
@@ -0,0 +1,14 @@
+fruits = ['orange', 'apple', 'pear','banaba', 'kiwi', 'apple', 'banana']
+
+# Count number of fruits
+
+fruit_counts = {}
+
+for fruit in fruits:
+    if fruit in fruit_counts:
+        fruit_counts[fruit] += 1
+    else:
+        fruit_counts[fruit] = 1
+
+for fruit, count in fruit_counts.items():
+    print(f"{fruit}: {count}")
